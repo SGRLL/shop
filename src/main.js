96a14bb7bca327d339f39313d68c0c2058b515d7
沈乐 <3383444911@qq.com>
2023-08-17 21:16:56 +0800

    home图片懒加载

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 //引入初始化样式文件
 import '@/styles/common.scss'
+import { useIntersectionObserver } from '@vueuse/core'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -22,3 +23,24 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+//定义全局指令
+    //懒加载
+app.directive('img-lazy', {
+    mounted(el, binding) {
+        //el:指令指定的那个元素 img
+        //binding：binding.value 指令对于号后面绑定的表达式的值 图片URL
+        // console.log(el,binding.value);
+        const { stop } = useIntersectionObserver(
+            el,
+            ([{ isIntersecting }]) => {
+                console.log(isIntersecting)
+                if (isIntersecting) {
+                    // 进入视口区域
+                    el.src = binding.value
+                    stop()
+                }
+            },
+        )
+    }
+})
